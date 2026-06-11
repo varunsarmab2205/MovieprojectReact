670d3853ab8e3144dafacf672b4e6832c5d4aff2
varunsarmab2205 <varunsarmab22@gmail.com>
2026-06-11 12:27:42 +0530

    Fix deployment import paths

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import authReducer from "./authSlice";
 import favoriteReducer from "./FavoriteSlice";
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-Redux";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import { store } from "./Redux/store";
 
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
